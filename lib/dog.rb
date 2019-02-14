8e21e7093763609d2342b9c70e839e54b40fe754
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-14 05:57:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,7 +79,7 @@ class Dog
     sql = <<-SQL
       UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
-    DB[:conn].execute(sql, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
     
   end 
   
